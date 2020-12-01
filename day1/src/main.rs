5f3d873af81ac906d56977df1185ec0f138f8c5f
Benjamin Groeber <mail@benjamingroeber.com>
2020-12-01 16:42:08 +0100

    Add helper for reading input

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,26 +1,28 @@
 use itertools::Itertools;
+use helpers::read_line_usize_from_file;
 
-static TARGET_VALUE: usize = 2020;
 fn main() {
-    let numbers: Vec<usize> = INPUT.lines().map(|x| x.parse().unwrap()).collect();
+    let numbers = read_line_usize_from_file("assets/days/day1.txt").unwrap();
     // Part 1
     // Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
-    day1(&numbers, 2);
+    day1(&numbers, 2, 2020);
     // Part 2
     // Find _three_ numbers in your expense report that meet the same criteria
-    day1(&numbers, 3);
+    day1(&numbers, 3, 2020);
 }
 
-fn day1(input: &[usize], n: usize) {
-    let result = n_combination_target_sum(input, TARGET_VALUE, n).next();
-    let product = result
-        .iter()
-        .map(|v| v.iter().fold(1 as usize, |acc, x| acc * **x))
-        .next();
-    println!("Sum of {:?} = {:?}", result, product);
+fn day1(input: &[usize], n: usize, target: usize) {
+    // Get first n values resulting in sum equals target
+    if let Some(values) = n_combination_target_sum(input, target, n).next() {
+        // Multiply resulting values with each other
+        let product: usize = values.iter().copied().product();
+        println!("Sum of {:?} = {:?}", values, product);
+    } else {
+        println!("There are no {} numbers, resulting in {}", n, target);
+    }
 }
 
-fn n_combination_target_sum<'a>(
+fn n_combination_target_sum(
     input: &[usize],
     target: usize,
     n: usize,
@@ -28,207 +30,6 @@ fn n_combination_target_sum<'a>(
     input
         .iter()
         .combinations(n)
-        .filter(move |values| values.into_iter().map(|x| *x).sum::<usize>() == target)
+        // filter all combinations, where the sum of the elements is equal to `target`
+        .filter(move |values| values.iter().copied().sum::<usize>() == target)
 }
-
-static INPUT: &str = "1782
-1344
-1974
-1874
-1800
-1973
-1416
-1952
-1982
-1506
-1642
-1514
-1978
-1895
-1747
-1564
-1398
-1683
-1886
-1492
-1629
-1433
-295
-1793
-1740
-1852
-1697
-1471
-1361
-1751
-1426
-2004
-1763
-1663
-1742
-1666
-1733
-1880
-1600
-1723
-1478
-1912
-1820
-1615
-1875
-1547
-1554
-752
-1905
-1368
-954
-1425
-1391
-691
-1835
-744
-1850
-1713
-1995
-1926
-1817
-1774
-1986
-2010
-1427
-1609
-1927
-1362
-1420
-1722
-1590
-1925
-1617
-1434
-1826
-1636
-1687
-1946
-704
-1797
-1517
-1801
-1865
-1963
-1828
-1829
-1955
-1832
-1987
-1585
-1646
-1575
-1351
-1345
-1729
-1933
-1918
-1902
-1490
-1627
-1370
-1650
-1340
-1539
-1588
-1715
-1573
-1384
-1403
-1673
-1750
-1578
-1831
-1849
-1719
-1359
-2008
-1837
-1958
-480
-1388
-1770
-1999
-1066
-1730
-1541
-1802
-1962
-1891
-1816
-1505
-1665
-1551
-1954
-1378
-1998
-1612
-1544
-1953
-1502
-1888
-1655
-1614
-1903
-1675
-1498
-1653
-1769
-1863
-1607
-1945
-1651
-1558
-1777
-1460
-1711
-1677
-1988
-1441
-1821
-1867
-1656
-1731
-1885
-1482
-1439
-1990
-1809
-1794
-1951
-1858
-1969
-509
-1486
-1971
-1557
-1896
-1884
-1834
-1814
-1216
-1997
-1966
-1808
-1754
-1804
-1684
-2001
-1699
-1781
-1429
-1322
-1603
-1596
-1823
-1700
-1552
-1352
-1621
-1669
-";
---
+++ helpers/src/lib.rs
@@ -0,0 +1,37 @@
+use std::path::Path;
+use std::error::Error;
+use std::io::Read;
+use std::fs::File;
+
+pub fn read_file<T: AsRef<Path>>(filename: T) -> Result<String, Box<dyn Error>> {
+    let mut file = File::open(filename)?;
+
+    let mut content = String::new();
+    file.read_to_string(&mut content)?;
+    Ok(content)
+}
+
+pub fn read_line_usize_from_file<T: AsRef<Path> >(filename: T) -> Result<Vec<usize>, Box<dyn Error>> {
+    let numbers: Result<Vec<_>, _> = read_file(filename)?.lines().map(|l|l.parse()).collect();
+    Ok(numbers?)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    static TXT_CONTENT: &str = "Lorem ipsum
+Foo
+Bar";
+    #[test]
+    fn test_read_file() {
+        let content = read_file("assets/helpers/text.txt").unwrap();
+        assert_eq!(TXT_CONTENT, content)
+    }
+
+    #[test]
+    fn test_usize_from_file() {
+        let lines = read_line_usize_from_file("assets/helpers/usize.txt").unwrap();
+        assert_eq!(&vec![1,23,47,1_000_000], &lines)
+    }
+}
