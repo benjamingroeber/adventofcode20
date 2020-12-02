b3de0973e548ba7eb6e1eac1ecfd9d9e502f4eae
Benjamin Groeber <mail@benjamingroeber.com>
2020-12-02 22:07:36 +0100

    rustfmt

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,11 +2,11 @@ use lazy_static::lazy_static;
 use regex::Regex;
 use std::error::Error;
 
-use thiserror::Error;
-use std::num::ParseIntError;
 use crate::PolicyError::ParsePolicy;
+use std::num::ParseIntError;
+use thiserror::Error;
 
-fn main() -> Result<(), Box<dyn Error>>{
+fn main() -> Result<(), Box<dyn Error>> {
     let input = helpers::read_file("assets/days/day2.txt")?;
     let policies: Result<Vec<_>, _> = input.lines().map(|l| parse_policy(l)).collect();
     let policies = policies?;
@@ -66,35 +66,40 @@ impl<'a> Policy<'a> {
 
     fn is_day2_policy_fulfilled(&self) -> bool {
         let mut chars = self.password.chars();
-        // first position must exist, or false is returned
-        chars.nth(self.lower_bound - 1).map_or( false, |char_at_lower| {
-            // reuse the existing iterator by calculating the distance from the first position to
-            // the second
-            let distance = self.upper_bound - self.lower_bound - 1;
-            chars.nth(distance).map_or_else(
-                // second position must exist, or only the first requirement is considered
-                || char_at_lower == self.letter,
-                // if both exist, __exactly__ one of them must match the letter
-                |char_at_upper| {
-                    (char_at_lower == self.letter || char_at_upper == self.letter)
-                        && char_at_upper != char_at_lower
-                },
-            )
-        })
+        chars
+            .nth(self.lower_bound - 1)
+            // first position must exist, or false is returned
+            .map_or(false, |char_at_lower| {
+                // reuse the existing iterator by calculating the distance from the first position to
+                // the second
+                let distance = self.upper_bound - self.lower_bound - 1;
+                chars.nth(distance).map_or_else(
+                    // second position must exist, or only the first requirement is considered
+                    || char_at_lower == self.letter,
+                    // if both exist, __exactly__ one of them must match the letter
+                    |char_at_upper| {
+                        (char_at_lower == self.letter || char_at_upper == self.letter)
+                            && char_at_upper != char_at_lower
+                    },
+                )
+            })
     }
 }
 
 // Input each line contains `{lower_bound}-{upper_bound} {letter}: {password}`
 fn parse_policy(line: &str) -> Result<Policy, Box<dyn Error>> {
-    let dash = line.find('-').ok_or_else(||ParsePolicy(line.to_owned()))?;
-    let first_space = line.find(' ').ok_or_else(||ParsePolicy(line.to_owned()))?;
-    let colon = line.find(':').ok_or_else(||ParsePolicy(line.to_owned()))?;
+    let dash = line.find('-').ok_or_else(|| ParsePolicy(line.to_owned()))?;
+    let first_space = line.find(' ').ok_or_else(|| ParsePolicy(line.to_owned()))?;
+    let colon = line.find(':').ok_or_else(|| ParsePolicy(line.to_owned()))?;
 
-    let lower_bound = line[0..dash].parse::<usize>()?;
-    let upper_bound = line[dash+1..first_space].parse::<usize>()?;
-    let letter = line[first_space+1..colon].chars().next().ok_or_else(||ParsePolicy(line.to_owned()))?;
+    let lower_bound: usize = line[0..dash].parse()?;
+    let upper_bound: usize = line[dash + 1..first_space].parse()?;
+    let letter = line[first_space + 1..colon]
+        .chars()
+        .next()
+        .ok_or_else(|| ParsePolicy(line.to_owned()))?;
     // need to skip the space after the colon
-    let password = &line[colon+2..];
+    let password = &line[colon + 2..];
 
     let policy = Policy {
         lower_bound,
@@ -112,19 +117,32 @@ fn parse_policy_regex(line: &str) -> Result<Policy, PolicyError> {
         static ref RE: Regex = Regex::new(r#"^(?P<lower_bound>[0-9]+)-(?P<upper_bound>[0-9]+)\s(?P<letter>[A-z]):\s(?P<password>.*)"#).unwrap();
     }
 
-    let captures = RE.captures(line).ok_or_else(||PolicyError::ParsePolicy(line.to_owned()))?;
+    let captures = RE
+        .captures(line)
+        .ok_or_else(|| PolicyError::ParsePolicy(line.to_owned()))?;
     assert_eq!(captures.len(), 5);
 
-    let lower_bound: usize = captures.name("lower_bound").ok_or_else(||PolicyError::ParsePolicy(line.to_owned()))?.as_str().parse()?;
-    let upper_bound: usize = captures.name("upper_bound").ok_or_else(||PolicyError::ParsePolicy(line.to_owned()))?.as_str().parse()?;
+    let lower_bound: usize = captures
+        .name("lower_bound")
+        .ok_or_else(|| PolicyError::ParsePolicy(line.to_owned()))?
+        .as_str()
+        .parse()?;
+    let upper_bound: usize = captures
+        .name("upper_bound")
+        .ok_or_else(|| PolicyError::ParsePolicy(line.to_owned()))?
+        .as_str()
+        .parse()?;
     let letter = captures
         .name("letter")
-        .ok_or_else(||PolicyError::ParsePolicy(line.to_owned()))?
+        .ok_or_else(|| PolicyError::ParsePolicy(line.to_owned()))?
         .as_str()
         .chars()
         .next()
-        .ok_or_else(||PolicyError::ParsePolicy(line.to_owned()))?;
-    let password = captures.name("password").ok_or_else(||PolicyError::ParsePolicy(line.to_owned()))?.as_str();
+        .ok_or_else(|| PolicyError::ParsePolicy(line.to_owned()))?;
+    let password = captures
+        .name("password")
+        .ok_or_else(|| PolicyError::ParsePolicy(line.to_owned()))?
+        .as_str();
 
     let policy = Policy {
         lower_bound,
@@ -142,12 +160,15 @@ mod tests {
     #[test]
     fn test_parse_policy() {
         let policy = parse_policy("10-20 a: password").unwrap();
-        assert_eq!(policy, Policy{
-            lower_bound: 10,
-            upper_bound: 20,
-            letter: 'a',
-            password: "password"
-        })
+        assert_eq!(
+            policy,
+            Policy {
+                lower_bound: 10,
+                upper_bound: 20,
+                letter: 'a',
+                password: "password"
+            }
+        )
     }
 
     #[test]
