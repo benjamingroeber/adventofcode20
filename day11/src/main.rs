28a300bfd4fad0edbc6a4bb04b5f6e14e4e7a8fa
Benjamin Groeber <mail@benjamingroeber.com>
2020-12-12 15:06:53 +0100

    Day 11 naive solution

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,11 +1,12 @@
-use helpers::read_file;
+use helpers::{read_file, Grid};
 use itertools::Itertools;
 use std::convert::TryFrom;
 use std::error::Error;
 use thiserror::Error;
 use std::fmt::{Display, Formatter, Write};
-use crate::Tile::SeatTaken;
+use crate::Tile::{SeatTaken, Floor};
 
+// TODO Refactor second part
 fn main() -> Result<(), Box<dyn Error>> {
     let input = read_file("./assets/days/day11.txt")?;
     let tiles = parse_grid(&input);
@@ -13,7 +14,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut ferry = Ferry { grid: tiles? };
     let mut i = 0;
     loop {
-        // println!("{}\n{}", ferry.grid, i);
         let result  = ferry.apply_rules_to_self();
         i += 1;
 
@@ -21,11 +21,44 @@ fn main() -> Result<(), Box<dyn Error>> {
             break
         }
     }
-    let count_seats_occupied = ferry.grid.items.iter().filter(|s|**s==SeatTaken).count();
-    println!("Occupied seats after {} iterations: {}", i, count_seats_occupied);
+    let count_seats_occupied = ferry.grid.iter().filter(|s|**s==SeatTaken).count();
+    println!("Part1: Occupied seats after {} iterations: {}", i, count_seats_occupied);
+    //
+    let tiles = parse_grid(&input);
+    let mut ferry = Ferry { grid: tiles? };
+    // let result = run_until_stable(&ferry, Box::new(Ferry::apply_rules_part2) as Box<dyn Iterator<Item=_>>);
+    let mut i = 0;
+    loop {
+        let result  = ferry.apply_rules2_to_self();
+        i += 1;
+
+        if result == SeatingRules::NoChange {
+            break
+        }
+    }
+    let count_seats_occupied = ferry.grid.iter().filter(|s|**s==SeatTaken).count();
+    println!("Part2: Occupied seats after {} iterations: {}", i, count_seats_occupied);
 
     Ok(())
 }
+
+/*fn run_until_stable<F>(ferry: &Ferry, f:F) ->  Ferry
+    where F: Fn(&Ferry) -> dyn Iterator<Item=((usize, usize), Tile)> {
+    let mut init = ferry.clone();
+    loop {
+        let mut new = init.grid.clone();
+
+        let iter = &f(ferry);
+        for ((col, row), tile) in iter.iter() {
+            new.set(col, row, tile)
+        }
+        if init.grid == new {
+            return init
+        } else {
+            init.grid = new;
+        }
+    }
+}*/
 
 #[derive(Clone,Debug, Eq, PartialEq)]
 struct Ferry {
@@ -52,8 +85,117 @@ impl Ferry {
             })
     }
 
-    fn apply_rules(&self) -> impl Iterator<Item = Tile> + '_ {
-        self.grid.items().map(move |((col, row), tile)| match tile {
+    fn visible_seats(&self, col: usize, row: usize) -> Vec<Tile> {
+        let mut tiles = Vec::new();
+        // same row, horizontal left
+        let mut i = 1;
+        while i <= col {
+            let tile = self.get_tile(col-(i), row);
+            if let Some(tile) = tile {
+                if tile != Floor {
+                    tiles.push(tile);
+                    break
+                }
+            }
+            i+=1
+        }
+
+        // same column, vertical up
+        let mut i = 1;
+        while i <= row {
+            let tile = self.get_tile(col, row-i);
+            if let Some(tile) = tile {
+                if tile != Floor {
+                    tiles.push(tile);
+                    break
+                }
+            }
+            i+=1
+        }
+
+        // diagonal up left
+        let mut i = 1;
+        // println!("AAAA {} {} {}", row, col, i);
+        while i <= row && i <= col {
+            let tile = self.get_tile(col-i, row-i);
+            if let Some(tile) = tile {
+                if tile != Floor {
+                    tiles.push(tile);
+                    break
+                }
+            }
+            i+=1
+        }
+
+        // diagonal up right
+        let mut i = 1;
+        while i <= row && col+i < self.grid.num_columns() {
+            let tile = self.get_tile(col+i, row-i);
+            if let Some(tile) = tile {
+                if tile != Floor {
+                    tiles.push(tile);
+                    break
+                }
+            }
+            i+=1
+        }
+
+        // same row horizontal right
+        let mut i = 1;
+        while col+i < self.grid.num_columns() {
+            let tile = self.get_tile(col+i, row);
+            if let Some(tile) = tile {
+                if tile != Floor {
+                    tiles.push(tile);
+                    break
+                }
+            }
+            i+=1
+        }
+
+        // same column vertical down
+        let mut i = 1;
+        while row+i < self.grid.num_rows() {
+            let tile = self.get_tile(col, row+i);
+            if let Some(tile) = tile {
+                if tile != Floor {
+                    tiles.push(tile);
+                    break
+                }
+            }
+            i+=1
+        }
+
+        // diagonal down right
+        let mut i = 1;
+        while col+i < self.grid.num_columns() && row+i < self.grid.num_rows() {
+            let tile = self.get_tile(col+i, row+i);
+            if let Some(tile) = tile {
+                if tile != Floor {
+                    tiles.push(tile);
+                    break
+                }
+            }
+            i+=1
+        }
+
+        // diagonal down left
+        let mut i = 1;
+        while i <= col && row+i < self.grid.num_rows() {
+            let tile = self.get_tile(col-i, row+i);
+            if let Some(tile) = tile {
+                if tile != Floor {
+                    tiles.push(tile);
+                    break
+                }
+            }
+            i+=1
+        }
+        tiles
+    }
+
+    fn apply_rules(&self) -> impl Iterator<Item = ((usize, usize),Tile)> + '_ {
+        self.grid.items_iter().map(move |((col, row), tile)| { let tile = match tile {
             Tile::Floor => *tile,
             Tile::SeatEmpty => {
                 if ! self.neighbours(col, row).any(|n| n == Tile::SeatTaken) {
@@ -68,14 +210,49 @@ impl Ferry {
                 } else {
                     *tile
                 }
+            }};
+            ((col, row), tile)
+        })
+    }
+
+    fn apply_rules_part2(&self) -> impl Iterator<Item = ((usize, usize),Tile)> + '_ {
+        self.grid.items_iter().map(move |((col, row), tile)| { let tile = match tile {
+            Tile::Floor => *tile,
+            Tile::SeatEmpty => {
+                if ! self.visible_seats(col, row).iter().any(|n| *n == Tile::SeatTaken) {
+                    Tile::SeatTaken
+                } else {
+                    *tile
+                }
             }
+            Tile::SeatTaken => {
+                if self.visible_seats(col, row).iter().filter(|n| **n == Tile::SeatTaken).count() >= 5 {
+                    Tile::SeatEmpty
+                } else {
+                    *tile
+                }
+            }};
+            ((col, row), tile)
         })
     }
 
     fn apply_rules_to_self(&mut self) -> SeatingRules {
         let mut new_grid = self.grid.clone();
-        for (i,tile) in self.apply_rules().enumerate() {
-            new_grid.items[i] = tile
+        for ((col, row),tile) in self.apply_rules() {
+            new_grid.set(col, row, tile)
+        }
+        if self.grid == new_grid {
+            SeatingRules::NoChange
+        } else {
+            self.grid = new_grid;
+            SeatingRules::Applied
+        }
+    }
+
+    fn apply_rules2_to_self(&mut self) -> SeatingRules {
+        let mut new_grid = self.grid.clone();
+        for ((col, row),tile) in self.apply_rules_part2() {
+            new_grid.set(col, row, tile)
         }
         if self.grid == new_grid {
             SeatingRules::NoChange
@@ -96,6 +273,7 @@ enum SeatingRules {
     NoChange
 }
 
+// TODO Refactor to Grid<Option<Seat>> with Seat::Empty and Seat::Occupied
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 enum Tile {
     Floor,
@@ -115,7 +293,7 @@ impl Display for Tile {
 }
 
 impl TryFrom<char> for Tile {
-    type Error = GridError;
+    type Error = FerryError;
 
     fn try_from(value: char) -> Result<Self, Self::Error> {
         match value {
@@ -123,87 +301,43 @@ impl TryFrom<char> for Tile {
             'L' => Ok(Tile::SeatEmpty),
             // replace with option?
             '.' => Ok(Tile::Floor),
-            _ => Err(GridError::UnknownTileError(value)),
+            _ => Err(FerryError::UnknownTile(value)),
         }
     }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq)]
-struct Grid<T> {
-    columns: usize,
-    items: Vec<T>,
-}
-
-impl<T> Grid<T> {
-    pub fn get(&self, col: usize, row: usize) -> Option<&T> {
-        // println!("{}", self.columns);
-        if col < self.columns {
-            self.items.get(row * self.columns + col)
-        } else { None }
-    }
-
-    pub fn get_unchecked(&self, col: usize, row: usize) -> &T {
-        if col < self.columns {
-            &self.items[row * self.columns + col]
-        } else {
-            panic!("Index out of bounds!")
-        }
-    }
-
-    pub fn items(&self) -> impl Iterator<Item = ((usize, usize), &T)> {
-        let rows = self.items.len() / self.columns;
-        (0..rows)
-            .cartesian_product(0..self.columns)
-            .map(move |(row, col)| ((col, row), self.get_unchecked(col, row)))
-    }
-}
-
-impl<T: Display> Display for Grid<T> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        for (i, item) in self.items.iter().enumerate() {
-            if i % self.columns == 0 {
-                writeln!(f, "")?;
-            }
-            write!(f, "{}", item)?;
-        }
-        Ok(())
-    }
-}
-
-fn parse_grid(s: &str) -> Result<Grid<Tile>, GridError> {
+fn parse_grid(s: &str) -> Result<Grid<Tile>, FerryError> {
     let mut common_length = None;
-    let mut tiles = Vec::with_capacity(s.len());
+    let mut items = Vec::with_capacity(s.len());
     for line in s.lines() {
         // sanity check for same length of all rows
         let line_length = line.chars().count();
         if let Some(size) = common_length {
-            // println!("'{}' {} {}", line, size, line_length);
             if size != line_length {
-                return Err(GridError::InconsistentXDimensionError);
+                return Err(FerryError::InconsistentXDimension);
             }
         } else {
             common_length = Some(line_length)
         }
         for tile in line.chars().map(Tile::try_from) {
-            tiles.push(tile?)
+            items.push(tile?)
         }
     }
 
-    Ok(Grid {
-        // if this is empty here, it means that there were no iterations on any lines before
-        columns: common_length.unwrap_or(0),
-        items: tiles,
-    })
+    // if common_length is empty here, it means that there were no iterations on any lines before
+    Ok(Grid::with_items(items, common_length.unwrap_or(0))?)
 }
 
 #[derive(Clone, Copy, Debug, Error)]
-enum GridError {
+enum FerryError {
     // #[error("could not parse Grid from string")]
     // ParseError,
+    #[error("grid error")]
+    Grid(#[from] helpers::GridError),
     #[error("not all lines have the same length")]
-    InconsistentXDimensionError,
+    InconsistentXDimension,
     #[error("unknown tile {0}, only '#', '.' and 'L' are allowed")]
-    UnknownTileError(char),
+    UnknownTile(char),
 }
 
 #[cfg(test)]
@@ -211,6 +345,7 @@ mod tests {
     use super::*;
     use crate::Tile::{Floor, SeatEmpty, SeatTaken};
 
+    // TODO move to helpers
     #[test]
     fn test_parse_grid() {
         let input = "LLL\n###\n...";
@@ -220,9 +355,10 @@ mod tests {
 
         let grid = parse_grid(&input).unwrap();
 
-        assert_eq!(grid.items, expected)
+        assert_eq!(grid.items(), expected)
     }
 
+    // TODO move to helpers
     #[test]
     fn test_grid_get() {
         let input = "LLL\n###\n...";
@@ -241,16 +377,16 @@ mod tests {
         assert_eq!(Some(&Floor), grid.get(2, 2));
     }
 
+    // TODO move to helpers
     #[test]
     fn test_grid_items() {
         let input = "LLL\n###\n...";
         let grid = parse_grid(&input).unwrap();
 
-        let items: Vec<_> = grid.items().map(|(_,tile)|*tile).collect();
-        let newgrid = Grid{items, columns: grid.columns};
-        println!("{}", grid);
-        println!("{}", newgrid);
-        assert_eq!(newgrid.items, grid.items)
+        let items: Vec<_> = grid.items_iter().map(|(_,tile)|*tile).collect();
+        let new_grid = Grid::with_items(items, grid.num_columns()).unwrap();
+
+        assert_eq!(new_grid.items(), grid.items())
     }
 
     #[test]
@@ -301,12 +437,43 @@ mod tests {
         let input = read_file("../assets/days/day11_example.txt").unwrap();
         let grid = parse_grid(&input).unwrap();
 
-        let ferry = Ferry{ grid };
+        let mut ferry = Ferry{ grid };
 
         // all seats are empty
-        assert!(ferry.grid.items().all(|(_,t)|*t==SeatEmpty||*t==Floor));
+        assert!(ferry.grid.items_iter().all(|(_,t)|*t==SeatEmpty||*t==Floor));
         // all seats are now filled
-        assert!(ferry.apply_rules().all(|t|t==SeatTaken||t==Floor));
+        assert!(ferry.apply_rules().all(|(_,t)|t==SeatTaken||t==Floor));
+
+        // after 5 rounds there are no more changes
+        for _ in 0..5 {
+            ferry.apply_rules_to_self();
+        }
+
+        let expected = ferry.clone();
+        ferry.apply_rules_to_self();
+        assert_eq!(expected, ferry)
+    }
+
+    // Now, instead of considering just the eight immediately adjacent seats, consider the first
+    // seat in each of those eight directions. For example, the empty seat below would see
+    // eight occupied seats:
+    // .......#.
+    // ...#.....
+    // .#.......
+    // .........
+    // ..#L....#
+    // ....#....
+    // .........
+    // #........
+    // ...#.....
+    #[test]
+    fn test_part2_visibility_example() {
+        let input = read_file("../assets/days/day11_example_part2.txt").unwrap();
+        let grid = parse_grid(&input).unwrap();
+
+        let ferry = Ferry{ grid };
+
+        assert_eq!(ferry.visible_seats(3,4).iter().filter(|t|**t==SeatTaken).count(), 8 );
     }
 
 }
--- helpers/src/lib.rs
+++ helpers/src/lib.rs
@@ -4,7 +4,10 @@ use std::io::Read;
 use std::path::Path;
 use std::str::FromStr;
 use thiserror::Error;
+use std::fmt::{Display, Formatter};
+use itertools::Itertools;
 
+// PARSING
 pub fn parse_lines_file<T: AsRef<Path>, F>(filename: T) -> Result<Vec<F>, HelperError>
 where
     F: FromStr,
@@ -36,12 +39,97 @@ pub fn read_line_usize_from_file<T: AsRef<Path>>(
     Ok(numbers?)
 }
 
+// GRID
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Grid<T> {
+    columns: usize,
+    items: Vec<T>,
+}
+
+impl<T> Grid<T> {
+   pub  fn with_items(items: Vec<T>, columns: usize) -> Result<Self, GridError> {
+        let len = items.len();
+        if len % columns == 0 {
+            Ok(Grid{ columns, items })
+        } else {
+            Err(GridError::UnevenRowsError(len, columns))
+        }
+    }
+
+    fn get_idx(&self, col: usize, row: usize) -> usize {
+        row * self.columns + col
+    }
+
+    pub fn get(&self, col: usize, row: usize) -> Option<&T> {
+        // println!("{}", self.columns);
+        if col < self.columns {
+            self.items.get(self.get_idx(col, row))
+        } else { None }
+    }
+
+    pub fn num_columns(&self) -> usize {
+        self.columns
+    }
+
+    pub fn num_rows(&self) -> usize {
+        self.items.len()/self.columns
+    }
+
+    pub fn set(&mut self, col: usize, row: usize, v: T) {
+        let idx = self.get_idx(col, row);
+        self.items[idx] = v
+    }
+
+    fn get_unchecked(&self, col: usize, row: usize) -> &T {
+        if col < self.columns {
+            &self.items[self.get_idx(col, row)]
+        } else {
+            panic!("Index out of bounds!")
+        }
+    }
+
+    pub fn items(&self) -> &[T] {
+        &self.items
+    }
+
+    pub fn items_iter(&self) -> impl Iterator<Item=((usize, usize), &T)> {
+        let rows = self.items.len() / self.columns;
+        (0..rows)
+            .cartesian_product(0..self.columns)
+            .map(move |(row, col)| ((col, row), self.get_unchecked(col, row)))
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item=&T> {
+        self.items.iter()
+    }
+}
+
+impl<T: Display> Display for Grid<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        for (i, item) in self.items.iter().enumerate() {
+            if i % self.columns == 0 {
+                writeln!(f)?;
+            }
+            write!(f, "{}", item)?;
+        }
+        Ok(())
+    }
+}
+
+// ERROR HANDLING
+
 #[derive(Error, Debug)]
 pub enum HelperError {
     #[error("could not open file")]
     IoError(#[from] std::io::Error),
     #[error("parsing error")]
     ParsingError,
+}
+
+#[derive(Clone, Copy, Debug, Error)]
+pub enum GridError {
+    #[error("{0} items can not be divided among {1} columns")]
+    UnevenRowsError(usize, usize)
 }
 
 #[cfg(test)]
