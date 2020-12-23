51e4d9e344fce26ca90015ef0b812d3558553db6
Benjamin Groeber <mail@benjamingroeber.com>
2020-12-24 00:31:13 +0100

    Day 23 Part 2

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,15 +1,30 @@
 use std::error::Error;
 use std::fmt::{Display, Formatter};
 use std::fs::read_to_string;
-use std::str::FromStr;
 use thiserror::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = read_to_string("./assets/days/day23.txt")?;
-    let mut cups = Cups::from_str(&input)?;
+    let labels = parse_labels(&input)?;
 
-    cups.play(100);
-    println!("Order: {}", cups.get_order_after_1());
+    // Part 1
+    {
+        let mut cups = Cups::with_labels(&labels);
+        cups.play(100);
+        println!("Order: {}", cups.get_order_after_1());
+    }
+
+    // Part 2
+    {
+        let mut cups = Cups::with_labels_and_fill(&labels, 1_000_000);
+        cups.play(10_000_000);
+
+        // Under the two cups that will end up immediately clockwise of cup 1. You can have them if
+        // you predict what the labels on those cups will be when the crab is finished.
+        // What do you get if you  multiply their labels together?
+        let (p1, p2) = cups.get_two_cups_after_one();
+        println!("P1: {} P2: {} Product: {}", p1, p2, p1 * p2)
+    }
     Ok(())
 }
 
@@ -44,6 +59,44 @@ impl Cups {
                 next,
             }
         }
+
+        // check for all between min and max for wrapping around
+        Cups { cur_pos, cups }
+    }
+
+    fn with_labels_and_fill(labels: &[Label], n: usize) -> Self {
+        // initialize all values
+        // here we're relying on the fact that labels contain all numbers from 1 to labels.len();
+        let mut cups = vec![Cup { label: 0, next: 0 }; n + 1];
+
+        let cur_pos = labels[0];
+        let mut max = 0_usize;
+        let mut numbers = labels.iter().peekable();
+        while let Some(label) = numbers.next() {
+            // track the maximum number to continue filling up from max +1
+            if *label > max {
+                max = *label
+            }
+            let next = if let Some(next) = &numbers.peek() {
+                ***next
+            } else {
+                // the last entry should point to the first number filling up
+                max + 1
+            };
+            cups[*label] = Cup {
+                label: *label,
+                next,
+            }
+        }
+        // fill up until the desired number of cups is reached
+        for i in max + 1..=n {
+            cups[i] = Cup {
+                label: i,
+                next: i + 1,
+            }
+        }
+        // adjust the last cup to point to the beginning
+        cups[n].next = cur_pos;
 
         // check for all between min and max for wrapping around
         Cups { cur_pos, cups }
@@ -106,6 +159,16 @@ impl Cups {
         }
         order
     }
+
+    // The crab is going to hide your stars - one each - under the two cups that will end up
+    // immediately clockwise of cup 1. You can have them if you predict what the labels on those
+    // cups will be when the crab is finished.
+    // Determine which two cups will end up immediately clockwise of cup 1.
+    fn get_two_cups_after_one(&self) -> (usize, usize) {
+        let p1 = self.get_next(1);
+        let p2 = self.get_next(p1);
+        (p1, p2)
+    }
 }
 
 impl Display for Cups {
@@ -120,17 +183,12 @@ impl Display for Cups {
     }
 }
 
-impl FromStr for Cups {
-    type Err = CupsError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let labels: Option<Vec<Label>> = s
-            .chars()
-            .map(|c| c.to_digit(10).map(|d| d as usize))
-            .collect();
-
-        Ok(Cups::with_labels(&labels.ok_or(CupsError::ParseCup)?))
-    }
+fn parse_labels(s: &str) -> Result<Vec<Label>, CupsError> {
+    let labels: Option<Vec<Label>> = s
+        .chars()
+        .map(|c| c.to_digit(10).map(|d| d as usize))
+        .collect();
+    Ok(labels.ok_or(CupsError::ParseCup)?)
 }
 
 #[derive(Clone, Debug, Error)]
@@ -146,7 +204,8 @@ mod tests {
     static TEST_INPUT: &str = "389125467";
     #[test]
     fn test_example_part1() {
-        let mut cups = Cups::from_str(TEST_INPUT).unwrap();
+        let labels = parse_labels(TEST_INPUT).unwrap();
+        let mut cups = Cups::with_labels(&labels);
         // For example, suppose your cup labeling were 389125467.
         // If the crab were to do merely 10 moves, the following changes would occur:
         cups.play(10);
@@ -166,5 +225,20 @@ mod tests {
 
         let order_100 = cups.get_order_after_1();
         assert_eq!(order_100, 67384529);
+    }
+
+    #[test]
+    fn text_example_part2() {
+        let labels = parse_labels(TEST_INPUT).unwrap();
+        let mut cups = Cups::with_labels_and_fill(&labels, 1_000_000);
+
+        cups.play(10_000_000);
+        // In the above example (389125467), this would be 934001 and then 159792;
+        let (p1, p2) = cups.get_two_cups_after_one();
+
+        assert_eq!(p1, 934001);
+        assert_eq!(p2, 159792);
+        // multiplying these together produces 149245887792.
+        assert_eq!(149245887792, p1 * p2);
     }
 }
